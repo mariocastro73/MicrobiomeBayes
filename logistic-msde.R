92b272e8c7a4779ca677ecda31a708fea81c574b
Mario Castro <marioc@comillas.edu>
2022-06-04 20:49:42 +0200

    Typos fixed

--- logistic-msde.R
+++ logistic-msde.R
@@ -3,7 +3,9 @@
 library(msde)
 library(GillespieSSA2)
 
-set.seed(1234)
+
+
+set.seed(123)
 source('auxfunctions.R')
 # Build model
 # put lotvolModel.h in the working directory
@@ -60,7 +62,23 @@ plot(x = 0, type = "n", xlim = range(tseq), ylim = c(0,max(Xobs)*1.2),
 
 
 x0 <- Xobs[1]
-### Aux function to do the posterior predictive plots
+
+# Funciones auxiliares (para pintar cosucas)
+colq <- function(X,q) apply(X, 2, function(X) as.numeric(quantile(X,q)))
+
+plotPostPred <- function(theta0,lwd=0.1,lty=2, col = 'orange') {
+  
+  logisticsim <- sde.sim(model = logisticmod, x0 = x0, theta = theta0,
+                         nobs = N-1, # N-1 steps forward
+                         dt = dT,
+                         dt.sim = dT/100) # internal observation time
+  
+  # plot data
+  Xsamp <- c(x0, logisticsim$data) # include first observation
+  lines(tseq, Xsamp,lwd=lwd,lty=lty,col=col)
+  return(Xsamp)
+}
+####
 
 temp <- c()
 for(i in 1:500) {
